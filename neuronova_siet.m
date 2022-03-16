b012dbf01a13daae66781ea7f3f64fcbfe4f0ea5
PeperonyPizza <xbrestovsky@stuba.sk>
2022-03-16 13:18:40 +0100

    TODO komentar

--- neuronova_siet.m
+++ neuronova_siet.m
@@ -7,7 +7,7 @@ function [output] = neuronova_siet(W1,W2,W3,snimace)
             O1 = tanh(A1);
             A2 = W2*O1;
             O2 = tanh(A2);
-            
+            %TODO: pridat rozsah -pi pi
             output = W3*O2;%-1,1 1:0.33 0.33:-0.33 -0.33:-1
 end
 
