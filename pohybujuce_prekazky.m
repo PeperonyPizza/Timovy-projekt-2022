f9bec226cfbcc0513585d7aa457161fcb5a0563e
xskundap <83547665+xskundap@users.noreply.github.com>
2022-06-07 07:59:43 +0200

    Doplnené popisy k funkciam, komenty, vymazane zbytočnosti

--- pohybujuce_prekazky.m
+++ pohybujuce_prekazky.m
@@ -32,24 +32,24 @@ function [new_pohybujuce_prekazky] = pohybujuce_prekazky (cesta,old_pohybujuce_p
                     
             end
 
-            if((cesta(new_pohybujuce_prekazky(i),new_pohybujuce_prekazky(i+1)) == 1) || (new_pohybujuce_prekazky(i)==pozicia(1) && new_pohybujuce_prekazky(i+1)==pozicia(2)))
-                new_pohybujuce_prekazky = old_pohybujuce_prekazky;
-            end
-            
-            % aby prekazka nesla mimo cesty alebo narazila do auta
-%             while (cesta(new_pohybujuce_prekazky(i),new_pohybujuce_prekazky(i+1)) == 1) || (new_pohybujuce_prekazky(i)==pozicia(1) && new_pohybujuce_prekazky(i+1)==pozicia(2))
-%                 pohyb_prekazky = randi(4);
+%             if((cesta(new_pohybujuce_prekazky(i),new_pohybujuce_prekazky(i+1)) == 1) || (new_pohybujuce_prekazky(i)==pozicia(1) && new_pohybujuce_prekazky(i+1)==pozicia(2)))
 %                 new_pohybujuce_prekazky = old_pohybujuce_prekazky;
-%                 switch pohyb_prekazky
-%                     case 1 
-%                         new_pohybujuce_prekazky(i) = new_pohybujuce_prekazky(i)+1;
-%                     case 2 
-%                         new_pohybujuce_prekazky(i) = new_pohybujuce_prekazky(i)-1;
-%                     case 3 
-%                         new_pohybujuce_prekazky(i+1) = new_pohybujuce_prekazky(i+1)+1;
-%                     case 4 
-%                         new_pohybujuce_prekazky(i+1) = new_pohybujuce_prekazky(i+1)-1;
-%                 end
-%             end 
+%             end
+            
+            %aby prekazka nesla mimo cesty alebo narazila do auta
+            while (cesta(new_pohybujuce_prekazky(i),new_pohybujuce_prekazky(i+1)) == 1) || (new_pohybujuce_prekazky(i)==pozicia(1) && new_pohybujuce_prekazky(i+1)==pozicia(2))
+                pohyb_prekazky = randi(4);
+                new_pohybujuce_prekazky = old_pohybujuce_prekazky;
+                switch pohyb_prekazky
+                    case 1 
+                        new_pohybujuce_prekazky(i) = new_pohybujuce_prekazky(i)+1;
+                    case 2 
+                        new_pohybujuce_prekazky(i) = new_pohybujuce_prekazky(i)-1;
+                    case 3 
+                        new_pohybujuce_prekazky(i+1) = new_pohybujuce_prekazky(i+1)+1;
+                    case 4 
+                        new_pohybujuce_prekazky(i+1) = new_pohybujuce_prekazky(i+1)-1;
+                end
+            end 
         end
 end
